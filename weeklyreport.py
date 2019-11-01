cc81f3c79a665102aabb6555e352bec831f4ebfa
heeneybd <55589166+heeneybd@users.noreply.github.com>
2019-11-01 13:59:58 +0800

    Add files via upload
    -added more possible sentence outputs.  
    -edited it so that listening and speaking comments are always combined into one sentence

--- weeklyreport.py
+++ weeklyreport.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
@@ -20,15 +19,15 @@ df = pd.read_excel(path, 'week4')
 df1 = pd.read_excel(path, 'student_names')
 
 ##These are the possible phrases that could be output (without names or pronouns)
-behavior1 = [" was very well behaved this week.", " has had good behavior this week.", " was a perfect little angel this week."]
-behavior2 = [" was fairly well behaved this week.", " did well in class this week.", " was pretty well-behaved this week."]
-behavior3 = [" was not very well behaved this week.", " showed poor behavior this week.", " was a little shit this week."]
-speaking1 = [" is able to speak quite well.", " has very good speaking skills and is able to make the sounds well.", " can speak well for this level."]
-speaking2 = [" is able to speak fairly well.", "'s speaking continues to show steady improvement.", " is able to say words and speak fairly well."]
-speaking3 = ["'s speaking needs improvement.", " still needs more practice speaking.", " needs more practice speaking loudly and clearly."]
-listening1 = [" has excellent listening skills and is able to clearly understand spoken English.", "'s listening skill is quite good.", " has excellent listening comprehension."]
-listening2 = ["'s listening skills continue to show improvement.", "'s listening skills are at the appropriate level.", " has no problem understanding spoken English."]
-listening3 = ["'s listening skills need improvement.", " needs more practice with listening.", " has difficulty understand spoken English."]
+behavior1 = [" was very well behaved this week.", " has had good behavior this week.", " was a perfect little angel this week.", " is very well-behaved and sets a positive example for other students in the class.", " had excellent behavior this week and followed directions well."]
+behavior2 = [" was fairly well behaved this week.", " did well in class this week.", " was pretty well-behaved this week.", " followed directions well this week and was fairly well-behaved.", " did well in class this week."]
+behavior3 = [" was not very well behaved this week.", " showed poor behavior this week.", " did not follow directions well this week.", " needs to work harder to follow the class rules.", " doesn't always listen to the teacher and follow the rules."]
+speaking1 = [" is able to speak quite well.", " has very good speaking skills and is able to make the sounds well.", " can speak well for this level.", " is able to speak English loudly and clearly.", " has excellent speaking skills."]
+speaking2 = [" is able to speak fairly well.", "'s speaking continues to show steady improvement.", " is able to say words and speak fairly well.", "'s speaking ability continues to show progress and improvement.", " is getting more comfortable using everyday English."]
+speaking3 = ["'s speaking could use practice.", " still needs more practice speaking.", " needs more practice speaking loudly and clearly.", "'s speaking still needs more practice.", " lacks confidence when speaking English.", "'s speaking skill needs extra practice."]
+listening1 = [" has excellent listening skills and is able to clearly understand spoken English.", "'s listening skill is quite good.", " has excellent listening comprehension.", " has good listening skills and can understand English well for this level.", "'s listening ability is quite good."]
+listening2 = ["'s listening skills continue to show improvement.", "'s listening skills are at the appropriate level.", " has no problem understanding spoken English.", " has no difficulty understanding basic English.", "'s listening ability continues to show improvement."]
+listening3 = ["'s listening skills need improvement.", " needs more practice with listening.", " has difficulty understand spoken English.", "'s listening skills still need improvement.", " needs more practice listening for English comprehension."]
 
 ##These lists qualifies the different strings as having positive or negative value 
 ##so the program knows to combine using "but" or "and" when constructing the comments.
@@ -73,43 +72,42 @@ for i in df.index:
     else:
         listeningstring = random.choice(listening3)
 
-##this changes the order of the sentences in the paragraph 
+##this changes the order of the sentences in the listening_speaking 
 ##and combines sentences 2 and 3 with "and" or "but" depending on whether
 ##they qualify as positive or negative        
-    paragraph = [listeningstring, speakingstring, behaviorstring]
-    random.shuffle(paragraph)
+    listening_speaking = [listeningstring, speakingstring]
+    random.shuffle(listening_speaking)
     
-    if paragraph[1] in positive:
-        if paragraph[2] in positive:
-            paragraph[1] = paragraph[1][:-1] + " and"
+    if listening_speaking[0] in positive:
+        if listening_speaking[1] in positive:
+            listening_speaking[0] = listening_speaking[0][:-1] + " and"
             
-        elif paragraph[2] in negative:
-            paragraph[1] = paragraph[1][:-1] + " but"
-    elif paragraph[1] in negative:
-        if paragraph[2] in positive:
-            paragraph[1] = paragraph[1][:-1] + " but"
-        elif paragraph[2] in negative:
-            paragraph[1] = paragraph[1][:-1] + " and"
-    paragraph[0] = student_name + paragraph[0]
-    paragraph[1] = student_pronoun + paragraph[1]
+        elif listening_speaking[1] in negative:
+            listening_speaking[0] = listening_speaking[0][:-1] + " but"
+    elif listening_speaking[0] in negative:
+        if listening_speaking[1] in positive:
+            listening_speaking[0] = listening_speaking[0][:-1] + " but"
+        elif listening_speaking[1] in negative:
+            listening_speaking[0] = listening_speaking[0][:-1] + " and"
+    behaviorstring = student_name + behaviorstring + "  "
+    listening_speaking[0] = student_pronoun + listening_speaking[0]
 
-    paragraph[2] = student_pronoun + paragraph[2]
-    paragraph[2] = paragraph[2][0].lower() + paragraph[2][1:]
+    listening_speaking[1] = student_pronoun + listening_speaking[1]
+    listening_speaking[1] = listening_speaking[1][0].lower() + listening_speaking[1][1:]
     
 
    
-    paragraph = ' '.join(paragraph)   
-    paragraph = paragraph.replace("She's", "Her")
-    paragraph = paragraph.replace("He's", "His")
-    paragraph = paragraph.replace("she's", "her")
-    paragraph = paragraph.replace("he's", "his")
+    listening_speaking = ' '.join(listening_speaking)   
+    listening_speaking = listening_speaking.replace("She's", "Her")
+    listening_speaking = listening_speaking.replace("He's", "His")
+    listening_speaking = listening_speaking.replace("she's", "her")
+    listening_speaking = listening_speaking.replace("he's", "his")
     
-    print(paragraph)
+    print(behaviorstring + listening_speaking)
     print('')
-    list.append(paragraph)
+    list.append(behaviorstring + listening_speaking)
     
 df = pd.DataFrame({'Rabbit Class Comments': list})
 writer = pd.ExcelWriter(r'C:\Users\braden.h\Desktop\ %s comments.xlsx' % date.today(), engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Weekly Comments')
 writer.save()
-
